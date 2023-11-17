082b52c5fa5119040468c7f2ce8031eb140fb62d
moohat <refixzcorp@gmail.com>
2023-11-17 09:42:37 -0800

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory(10)->create();
+
+        \App\Models\User::create([
+            'name' => 'Test user',
+            'email' => 'fic10@gail.com',
+            'password' => Hash::make('12316'),
+            'roles' => 'ADMIN',
+            'phone'=> '082123822',
+        ]);
+
+
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('pages.auth.login');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.dashboard');
+    })->name('home');
+
+    // Route::get('/users', function () {
+    //     return view('pages.users.index');
+    // })->name('users');
+});
+
+// Route::get('/login', function () {
+//     return view('pages.auth.login');
+// })->name('login');
+
+// //register
+// Route::get('/register', function () {
+//     return view('pages.auth.register');
+// })->name('register');
+
+// //users
+// Route::get('/users', function () {
+//     return view('pages.users.index');
+// });
